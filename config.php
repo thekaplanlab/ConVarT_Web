604558fa2a4a71f3ca5a51f485293997df687689
Ahmet Sayıcı <sayiciahmet@gmail.com>
2021-12-16 03:43:35 +0300

    first commit of empty config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
+$GLOBALS['base_url'] = '';
+$GLOBALS['host'] =  "";
+$GLOBALS['user'] = "";
+$GLOBALS['password'] = "";
+$GLOBALS['dbname'] = "";
